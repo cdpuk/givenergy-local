0734f10ba2dbf90bbc6311d05d5dc8a72f39537b
Chris Peka <24318424+cdpuk@users.noreply.github.com>
2022-07-13 19:17:10 +0000

    Fix tests, update README

--- custom_components/givenergy_local/config_flow.py
+++ custom_components/givenergy_local/config_flow.py
@@ -8,7 +8,6 @@ from givenergy_modbus.client import GivEnergyClient, Plant
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
-from homeassistant.exceptions import HomeAssistantError
 import voluptuous as vol
 
 from .const import CONF_HOST, CONF_NUM_BATTERIES, DOMAIN, LOGGER
@@ -18,21 +17,18 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 )
 
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> str:
     """Validate the user input allows us to connect.
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    try:
-        plant = Plant(number_batteries=data[CONF_NUM_BATTERIES])
-        client = GivEnergyClient(data[CONF_HOST])
-        async with async_timeout.timeout(10):
-            await hass.async_add_executor_job(client.refresh_plant, plant, True)
+    plant = Plant(number_batteries=data[CONF_NUM_BATTERIES])
+    client = GivEnergyClient(data[CONF_HOST])
+    async with async_timeout.timeout(10):
+        await hass.async_add_executor_job(client.refresh_plant, plant, True)
 
-        serial_no = plant.inverter.inverter_serial_number
-        return {"title": f"Solar Inverter (S/N {serial_no})", "host": data[CONF_HOST]}
-    except Exception as ex:  # pylint: disable=broad-except
-        raise CannotConnect from ex
+    serial_no: str = plant.inverter.inverter_serial_number
+    return serial_no
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call-arg]
@@ -52,19 +48,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call
         errors = {}
 
         try:
-            info = await validate_input(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
+            serial_no = await validate_input(self.hass, user_input)
         except Exception:  # pylint: disable=broad-except
-            LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
+            LOGGER.exception("Failed to validate inverter configuration")
+            errors["base"] = "cannot_connect"
         else:
-            return self.async_create_entry(title=info["title"], data=user_input)
+            return self.async_create_entry(
+                title=f"Solar Inverter (S/N {serial_no})", data=user_input
+            )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
