266075d4e0f8d52e6488474c98e0a37272a31b27
reidjr <reidjr@lineone.net>
2023-07-26 11:08:24 +0100

    Update __init__.py

--- custom_components/givenergy_local/__init__.py
+++ custom_components/givenergy_local/__init__.py
@@ -10,7 +10,6 @@ from .const import CONF_HOST, CONF_NUM_BATTERIES, DOMAIN, LOGGER
 from .coordinator import GivEnergyUpdateCoordinator
 from .services import async_setup_services, async_unload_services
 
-
 _PLATFORMS: list[Platform] = [
     Platform.BINARY_SENSOR,
     Platform.NUMBER,
