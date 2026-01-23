1825f0c0d653d5b2a612d348ca10993d3e7abc36
cdpuk <24318424+cdpuk@users.noreply.github.com>
2026-01-23 21:08:15 +0000

    Fix tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,10 @@
 """Test givenergy_local setup process."""
 
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-import pytest
+from homeassistant.config_entries import ConfigEntryState
+
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.givenergy_local import async_setup_entry
 from custom_components.givenergy_local.const import DOMAIN
 
 from .const import MOCK_CONFIG
@@ -13,10 +12,18 @@ from .const import MOCK_CONFIG
 
 async def test_setup_entry_exception(hass: HomeAssistant, error_on_get_data):
     """Test ConfigEntryNotReady when API raises an exception during entry setup."""
-    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
+    config_entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=MOCK_CONFIG,
+        entry_id="test",
+        version=2,
+    )
+    config_entry.add_to_hass(hass)
 
     # In this case we are testing the condition where async_setup_entry raises
     # ConfigEntryNotReady using the `error_on_get_data` fixture which simulates
     # an error.
-    with pytest.raises(ConfigEntryNotReady):
-        assert await async_setup_entry(hass, config_entry)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.SETUP_RETRY
